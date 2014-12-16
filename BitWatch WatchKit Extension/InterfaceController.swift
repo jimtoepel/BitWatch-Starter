67a82abefaa63457ba99e8a19d84ff6dd41e0544
jimtoepel <jim.toepel@gmail.com>
2014-12-15 22:50:03 -0800

    Initial Commit
    BasicWatchKit Tutorial

---
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.swift
+//  BitWatch WatchKit Extension
+//
+//  Created by Jim Toepel on 12/15/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        NSLog("%@ awakeWithContext", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
