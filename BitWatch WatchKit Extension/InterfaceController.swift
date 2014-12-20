2793da687d3b50ca7198f7d2588b290db43e32dd
jimtoepel <jim.toepel@gmail.com>
2014-12-20 15:16:03 -0800

    Finished the app
    Added updating, layout and quick-look assessment.

--- BitWatch WatchKit Extension/InterfaceController.swift
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -8,12 +8,20 @@
 
 import WatchKit
 import Foundation
-
+import BitWatchKit
 
 class InterfaceController: WKInterfaceController {
 
+    let tracker = Tracker()
+    var updating = false
+    
+    @IBOutlet weak var image: WKInterfaceImage!
     @IBOutlet weak var priceLabel: WKInterfaceLabel!
+    @IBOutlet weak var lastUpdatedLabel: WKInterfaceLabel!
+
+    
     @IBAction func refreshTapped() {
+        update()
     }
     
     override func awakeWithContext(context: AnyObject?) {
@@ -21,12 +29,16 @@ class InterfaceController: WKInterfaceController {
         
         // Configure interface objects here.
         NSLog("%@ awakeWithContext", self)
+        updatePrice(tracker.cachedPrice())
+        image.setHidden(true)
+        updateDate(tracker.cachedDate())
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         NSLog("%@ will activate", self)
+        update()
     }
 
     override func didDeactivate() {
@@ -34,5 +46,46 @@ class InterfaceController: WKInterfaceController {
         NSLog("%@ did deactivate", self)
         super.didDeactivate()
     }
+    
+    private func updatePrice(price: NSNumber) {
+        priceLabel.setText(Tracker.priceFormatter.stringFromNumber(price))
+    }
 
+    private func update() {
+        if !updating {
+            
+            updating = true
+            
+            let originalPrice = tracker.cachedPrice()
+            
+            tracker.requestPrice { (price, error ) -> () in
+                
+                if error == nil {
+                    self.updatePrice(price!)
+                    self.updateDate(NSDate())
+                    self.updateImage(originalPrice, newPrice: price!)
+                }
+                self.updating = false
+            }
+        }
+    }
+    
+    private func updateDate(date: NSDate) {
+        self.lastUpdatedLabel.setText("Last updated \(Tracker.dateFormatter.stringFromDate(date))")
+    }
+    
+    private func updateImage(originalPrice: NSNumber, newPrice: NSNumber) {
+        if originalPrice.isEqualToNumber(newPrice) {
+            
+            image.setHidden(true)
+        } else {
+            
+            if newPrice.doubleValue > originalPrice.doubleValue {
+                image.setImageNamed("Up")
+            } else {
+                image.setImageNamed("Down")
+            }
+            image.setHidden(false)
+        }
+    }
 }
